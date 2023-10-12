8677f3017039e3496f37dcdaa0cfce4ea94d9ff1
Gokul <jayavel.g@northeastern.edu>
2023-10-11 22:22:36 -0400

    modified code to work for 2 az

--- src/route-tables/index.ts
+++ src/route-tables/index.ts
@@ -1,3 +1,37 @@
+// import * as pulumi from "@pulumi/pulumi";
+// import * as aws from "@pulumi/aws";
+// import { vpc } from "../vpc";
+// import { publicSubnets, privateSubnets } from "../subnets";
+
+// // Creating a public route table
+// export const publicRouteTable = new aws.ec2.RouteTable("publicRouteTable", {
+//     vpcId: vpc.id
+// });
+
+// // Associate public subnets with public route table
+// publicSubnets.forEach((subnet, index) => {
+//     new aws.ec2.RouteTableAssociation(`publicRouteTableAssociation${index}`, {
+//         subnetId: subnet.id,
+//         routeTableId: publicRouteTable.id
+//     });
+// });
+
+// // Creating a private route table
+// export const privateRouteTable = new aws.ec2.RouteTable("privateRouteTable", {
+//     vpcId: vpc.id
+// });
+
+// // Associate private subnets with private route table
+// privateSubnets.forEach((subnet, index) => {
+//     new aws.ec2.RouteTableAssociation(`privateRouteTableAssociation${index}`, {
+//         subnetId: subnet.id,
+//         routeTableId: privateRouteTable.id
+//     });
+// });
+
+
+
+
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import { vpc } from "../vpc";
@@ -9,12 +43,14 @@ export const publicRouteTable = new aws.ec2.RouteTable("publicRouteTable", {
 });
 
 // Associate public subnets with public route table
-publicSubnets.forEach((subnet, index) => {
-    new aws.ec2.RouteTableAssociation(`publicRouteTableAssociation${index}`, {
-        subnetId: subnet.id,
-        routeTableId: publicRouteTable.id
-    });
-});
+publicSubnets.apply(subnets => 
+    subnets.forEach((subnet, index) => {
+        new aws.ec2.RouteTableAssociation(`publicRouteTableAssociation${index}`, {
+            subnetId: subnet.id,
+            routeTableId: publicRouteTable.id
+        });
+    })
+);
 
 // Creating a private route table
 export const privateRouteTable = new aws.ec2.RouteTable("privateRouteTable", {
@@ -22,9 +58,11 @@ export const privateRouteTable = new aws.ec2.RouteTable("privateRouteTable", {
 });
 
 // Associate private subnets with private route table
-privateSubnets.forEach((subnet, index) => {
-    new aws.ec2.RouteTableAssociation(`privateRouteTableAssociation${index}`, {
-        subnetId: subnet.id,
-        routeTableId: privateRouteTable.id
-    });
-});
+privateSubnets.apply(subnets => 
+    subnets.forEach((subnet, index) => {
+        new aws.ec2.RouteTableAssociation(`privateRouteTableAssociation${index}`, {
+            subnetId: subnet.id,
+            routeTableId: privateRouteTable.id
+        });
+    })
+);
--- src/subnets/index.ts
+++ src/subnets/index.ts
@@ -1,3 +1,37 @@
+// import * as pulumi from "@pulumi/pulumi";
+// import * as aws from "@pulumi/aws";
+// import { vpc } from "../vpc";
+
+// const config = new pulumi.Config();
+// const publicSubnetCidrBlocks = config.requireObject<string[]>("publicSubnetCidrBlocks");
+// const privateSubnetCidrBlocks = config.requireObject<string[]>("privateSubnetCidrBlocks");
+
+// // Using pulumi.Output to handle the results of the promise
+// const availabilityZones = aws.getAvailabilityZones({}).then(az => az.names);
+
+// console.log(availabilityZones);
+
+// // Creating public subnets
+// export const publicSubnets = publicSubnetCidrBlocks.map((cidrBlock, index) => 
+//     new aws.ec2.Subnet(`publicSubnet${index}`, {
+//         cidrBlock: cidrBlock,
+//         vpcId: vpc.id,
+//         availabilityZone: pulumi.output(availabilityZones).apply(az => az[index]),
+//         mapPublicIpOnLaunch: true
+//     })
+// );
+
+// // Creating private subnets
+// export const privateSubnets = privateSubnetCidrBlocks.map((cidrBlock, index) => 
+//     new aws.ec2.Subnet(`privateSubnet${index}`, {
+//         cidrBlock: cidrBlock,
+//         vpcId: vpc.id,
+//         availabilityZone: pulumi.output(availabilityZones).apply(az => az[index]),
+//         mapPublicIpOnLaunch: false
+//     })
+// );
+
+
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import { vpc } from "../vpc";
@@ -6,25 +40,36 @@ const config = new pulumi.Config();
 const publicSubnetCidrBlocks = config.requireObject<string[]>("publicSubnetCidrBlocks");
 const privateSubnetCidrBlocks = config.requireObject<string[]>("privateSubnetCidrBlocks");
 
-// Using pulumi.Output to handle the results of the promise
-const availabilityZones = aws.getAvailabilityZones({}).then(az => az.names);
+// Properly handling Pulumi Output for availability zones
+const availabilityZones = pulumi.output(aws.getAvailabilityZones({}).then(az => az.names));
+
+availabilityZones.apply(az => console.log(az));
+
+// Determine number of subnets to create based on availability zones
+const subnetCount = availabilityZones.apply(az => az.length <= 2 ? 2 : 3);
 
 // Creating public subnets
-export const publicSubnets = publicSubnetCidrBlocks.map((cidrBlock, index) => 
-    new aws.ec2.Subnet(`publicSubnet${index}`, {
-        cidrBlock: cidrBlock,
-        vpcId: vpc.id,
-        availabilityZone: pulumi.output(availabilityZones).apply(az => az[index]),
-        mapPublicIpOnLaunch: true
-    })
-);
+export const publicSubnets = pulumi.all([publicSubnetCidrBlocks, subnetCount, availabilityZones])
+    .apply(([cidrBlocks, count, az]) => 
+        cidrBlocks.slice(0, count).map((cidrBlock, index) => 
+            new aws.ec2.Subnet(`publicSubnet${index}`, {
+                cidrBlock: cidrBlock,
+                vpcId: vpc.id,
+                availabilityZone: az[index],
+                mapPublicIpOnLaunch: true
+            })
+        )
+    );
 
 // Creating private subnets
-export const privateSubnets = privateSubnetCidrBlocks.map((cidrBlock, index) => 
-    new aws.ec2.Subnet(`privateSubnet${index}`, {
-        cidrBlock: cidrBlock,
-        vpcId: vpc.id,
-        availabilityZone: pulumi.output(availabilityZones).apply(az => az[index]),
-        mapPublicIpOnLaunch: false
-    })
-);
+export const privateSubnets = pulumi.all([privateSubnetCidrBlocks, subnetCount, availabilityZones])
+    .apply(([cidrBlocks, count, az]) => 
+        cidrBlocks.slice(0, count).map((cidrBlock, index) => 
+            new aws.ec2.Subnet(`privateSubnet${index}`, {
+                cidrBlock: cidrBlock,
+                vpcId: vpc.id,
+                availabilityZone: az[index],
+                mapPublicIpOnLaunch: false
+            })
+        )
+    );
