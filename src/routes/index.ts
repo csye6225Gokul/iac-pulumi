5cf6426e563c1966a7520ba921b6e2069bf71157
Gokul Jayavel <112915309+gokul-j99@users.noreply.github.com>
2023-10-18 12:53:32 -0400

    Devlopment (#3)
    * added code for dev profile
    
    * added readme and demo yaml
    
    * added readme and demo yaml
    
    * added main index
    
    * modified code to work for 2 az
    
    * modified code
    
    * added code
    
    * added code
    
    * added security grp and ec2
    
    * updated yaml
    
    * updated variables
    
    * updated variables

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,8 +3,11 @@ import * as aws from "@pulumi/aws";
 import { publicRouteTable } from "../route-tables";
 import { internetGateway } from "../igw";
 
+const config = new pulumi.Config();
+const cidrBlock = config.require("igwcidrBlock");
+
 export const publicRoute = new aws.ec2.Route("publicRoute", {
     routeTableId: publicRouteTable.id,
-    destinationCidrBlock: "0.0.0.0/0",
+    destinationCidrBlock: cidrBlock,
     gatewayId: internetGateway.id
 });
